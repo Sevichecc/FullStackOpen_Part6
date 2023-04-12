6d9ecdeb6f230f978c944a577bbc10ddc6af9145
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-04-13 01:11:25 +0800

    Finish Part 6.4

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -4,7 +4,7 @@ const anecdotesAtStart = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
 ]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
@@ -13,22 +13,38 @@ const asObject = (anecdote) => {
   return {
     content: anecdote,
     id: getId(),
-    votes: 0
+    votes: 0,
+  }
+}
+
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    payload: {
+      content,
+      id: getId(),
+      votes: 0,
+    },
   }
 }
 
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-
-  if (action.type === 'ADD_VOTE') {
-    const id = action.payload.id
-    const anecdoteToVote = state.find(a => a.id === id)
-    const changeAnecdotes = 
-      { ...anecdoteToVote, votes: anecdoteToVote.votes + 1 }
-    return state.map(a=> a.id=== id ? changeAnecdotes : a)
+  switch (action.type) {
+    case 'NEW_ANECDOTE':
+      return [...state, action.payload]
+    case 'ADD_VOTE':
+      const id = action.payload.id
+      const anecdoteToVote = state.find((a) => a.id === id)
+      const changeAnecdotes = {
+        ...anecdoteToVote,
+        votes: anecdoteToVote.votes + 1,
+      }
+      return state.map((a) => (a.id === id ? changeAnecdotes : a))
+    default:
+      return state
   }
-  return state
 }
 
 export default reducer
