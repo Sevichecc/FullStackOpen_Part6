94b0a2a58661715a460c3bd5e10d9afb3d385576
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-04-23 12:22:48 +0800

    Finish Part 6.20

--- query-anecdotes/src/index.js
+++ query-anecdotes/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { QueryClient, QueryClientProvider } from 'react-query'
 import App from './App'
 
+const queryCLient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <QueryClientProvider client={queryCLient}>
+    <App />
+  </QueryClientProvider>
 )
